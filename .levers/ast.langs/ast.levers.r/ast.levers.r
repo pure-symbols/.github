2f538e24daf8ed72c09d134f0a8846b040e42653
ypa y yhm <103625580+yhm-amber@users.noreply.github.com>
2023-11-22 12:27:01 +0800

    Update ast.levers.r

--- .levers/ast.langs/ast.levers.r/ast.levers.r
+++ .levers/ast.langs/ast.levers.r/ast.levers.r
@@ -1,17 +1,16 @@
 # trans quoted "call"s to ast "list"
 codes.call.ast = 
 \ (callings) callings |> 
-	as.list () |> 
-	lapply (\ (x) 
-		if (is.call(x)) codes.call.ast (x) else x) ;
+as.list () |> lapply (\ (x) 
+	if (is.call(x)) codes.call.ast (x) else x) ;
 
 # trans ast "list" to quoted "call"s
 codes.ast.call = 
 \ (ast) ast |> 
-	lapply (\ (xs) 
-		if (list.have.nest (xs)) codes.ast.call (xs) else 
-		if (is.list (xs)) as.call (xs) else xs) |> 
-	as.call() ;
+lapply (\ (xs) 
+	if (list.have.nest (xs)) codes.ast.call (xs) else 
+	if (is.list (xs)) as.call (xs) else xs) |> 
+as.call() ;
 
 
 
@@ -20,8 +19,8 @@ codes.ast.call =
 # to see does a list have a sub list
 list.have.nest <- 
 \ (lst) lst |> 
-	lapply (\ (x) is.list(x)) |> 
-	unlist () |> any () ;
+lapply (\ (x) is.list(x)) |> 
+unlist () |> any () ;
 
 
 
@@ -29,24 +28,27 @@ list.have.nest <-
 # trans elements in ast "list" by f
 codes.ast.deeplapply.element = 
 \ (ast, f) ast |> 
-	lapply (\ (x) 
-		if (is.list (x)) 
-		codes.ast.deeplapply.element (x, f) else 
-		f (x)) ;
+lapply (\ (x) 
+	if (is.list (x)) 
+	codes.ast.deeplapply.element (x, f) else 
+	f (x)) ;
 
 # trans elements in quoted "call"s by f
 codes.call.trans.element = 
-\ (callings, f = \ (x) x) callings |> 
-	codes.call.ast () |> 
-	codes.ast.deeplapply.element (f) |> 
-	codes.ast.call () ;
+\ (callings, f = \ (x) x) 
+callings |> 
+codes.call.ast () |> 
+codes.ast.deeplapply.element (f) |> 
+codes.ast.call () ;
 
 
 
 
 # trans asts in ast "list" by f
 codes.ast.deeplapply.ast = 
-\ (ast, f) ast |> lapply (\ (xs) 
+\ (ast, f, f.ast.trees = f, f.ast.leaves = f, f.element.all = \ (x) x) 
+ast |> 
+lapply (\ (xs) 
 	if (list.have.nest (xs)) 
 	xs |> codes.ast.deeplapply.ast (f) else 
 	if (is.list (xs)) 
@@ -55,9 +57,9 @@ codes.ast.deeplapply.ast =
 # trans asts in quoted "call"s by f
 codes.call.trans.ast = 
 \ (callings, f = \ (a) a) callings |> 
-	codes.call.ast () |> 
-	codes.ast.deeplapply.ast (f) |> 
-	codes.ast.call () ;
+codes.call.ast () |> 
+codes.ast.deeplapply.ast (f) |> 
+codes.ast.call () ;
 
 
 
@@ -70,7 +72,6 @@ codes.src.call =
 
 # trans "call"s to "expression"
 codes.call.expression = 
-\ (callings, f) callings |> 
-	as.expression () ;
+\ (callings, f) callings |> as.expression () ;
 
 
